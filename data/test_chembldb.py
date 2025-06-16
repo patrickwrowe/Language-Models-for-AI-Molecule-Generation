3ad9330dbcd7fd465f2e7752e740ac7c15fa9a11
Patrick Rowe <patrick.william.rowe@gmail.com>
2025-06-16 09:04:32 -0230

    passing tests for new function

--- data/test_chembldb.py
+++ data/test_chembldb.py
@@ -5,16 +5,22 @@ import pytest
 def chembldb_small():
     return ChemblDB()._load_or_download(nrows=25)
 
+@pytest.fixture(scope='session')
+def chembldb_small_preprocessed(chembldb_small):
+    return ChemblDB()._preprocess(chembldb_small, column="canonical_smiles")
+
 def test_chembldb_load(chembldb_small):
     assert len(chembldb_small) == 25
 
-@pytest.fixture(scope='session')
-def test_chembldb_preprocess(chembldb_small):
-    text_preprocessed = ChemblDB()._preprocess(chembldb_small, column="canonical_smiles")
-    assert len(text_preprocessed) > 1
-    return text_preprocessed
+def test_chembldb_preprocess(chembldb_small_preprocessed, tmp_path):
+    assert len(chembldb_small_preprocessed) > 1
 
-def test_tokenize(test_chembldb_preprocess):
-    tokens = ChemblDB()._tokenize(test_chembldb_preprocess)
+def test_tokenize(chembldb_small_preprocessed, tmp_path):   
+    tmp_txt_fn = "temp_chembldb_text.txt"
+
+    with open(tmp_path / tmp_txt_fn, 'w') as f:
+        f.write(chembldb_small_preprocessed)
+
+    tokens = ChemblDB()._tokenize(str(tmp_path / tmp_txt_fn))
     assert tokens
 
