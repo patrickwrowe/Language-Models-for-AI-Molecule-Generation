082d22df59e65ec029b574ae6e201245117d3785
Patrick Rowe <patrick.william.rowe@gmail.com>
2025-07-17 09:25:26 +0100

    add dataloader methods to dataset class

--- data/smiles_dataset.py
+++ data/smiles_dataset.py
@@ -1,5 +1,6 @@
 from transformers.tokenization_utils_fast import PreTrainedTokenizerFast
 from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 import torch
 import attr
 
@@ -26,10 +27,14 @@ class SMILESDataset(Dataset):
     # Max length of any individual SMILES id
     max_length: int = 2048
 
+    # Batching and splitting
+    batch_size: int = 64
+
+
     def __len__(self):
         return len(self.smiles_list)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int):
         smiles = self.smiles_list[idx]
 
         # Seems super inefficient to encode each item one-by-one...
@@ -48,9 +53,17 @@ class SMILESDataset(Dataset):
             num_classes=len(self.tokenizer)
         )
 
-
-
         return one_hot
     
+    def get_dataloader(self, train: bool):
+        return DataLoader(
+            self,
+            batch_size = self.batch_size,
+            shuffle = train
+        )
 
+    def train_dataloader(self):
+        return self.get_dataloader(train=True)
 
+    def val_dataloader(self):
+        return self.get_dataloader(train=False)
