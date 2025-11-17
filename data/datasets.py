cec759e3179f10b6e5668bb6b6a924348662bf83
Patrick Rowe <patrick.william.rowe@gmail.com>
2025-11-17 12:44:55 -0330

    refactor datasets to avoid automatically loading with potentially missing path, which broke module loading

--- data/datasets.py
+++ data/datasets.py
@@ -15,119 +15,122 @@ from torch.utils.data import Dataset, DataLoader, Subset
 import torch
 import attr
 
-# @attr.define
-# class ChEMBLIndicationsExtended(Dataset):
-#     """
-#     Character level SMILES database with disease indications
-#     """
+@attr.define
+class ChEMBLIndicationsExtended(Dataset):
+    """
+    Character level SMILES database with disease indications
+    """
 
-#     # Length and batch size for loading
-#     max_length: int = 512
+    # ChemblDBIndications()._preprocess(ChemblDBIndications()._load_data(), max_length=max_length)
+    drug_data: pd.DataFrame
 
-#     drug_data: pd.DataFrame = ChemblDBIndications()._preprocess(ChemblDBIndications()._load_data(), max_length=max_length)
-#     chemreps_data: list[str] = ChemblDBChemreps()._preprocess(ChemblDBChemreps()._load_or_download(), continuous=False)[::10]
+    #  ChemblDBChemreps()._preprocess(ChemblDBChemreps()._load_or_download(), continuous=False)[::10]
+    chemreps_data: list[str]
+
+    # Length and batch size for loading
+    max_length: int = 512
     
-#     batch_size: int = 64
-#     frac_train: float = 0.8
+    batch_size: int = 64
+    frac_train: float = 0.8
 
-#     smiles_column_title = "canonical_smiles"
+    smiles_column_title = "canonical_smiles"
 
-#     def __attrs_post_init__(self):        
-#         self.chemreps_df = pd.DataFrame(
-#             data={
-#                 self.smiles_column_title: self.chemreps_data,
-#                 "mesh_heading_Other": [True] * len(self.chemreps_data)
-#             }
-#         )
+    def __attrs_post_init__(self):        
+        self.chemreps_df = pd.DataFrame(
+            data={
+                self.smiles_column_title: self.chemreps_data,
+                "mesh_heading_Other": [True] * len(self.chemreps_data)
+            }
+        )
 
-#         self.all_data = pd.concat([self.drug_data, self.chemreps_df]).fillna(0)
+        self.all_data = pd.concat([self.drug_data, self.chemreps_df]).fillna(0)
         
-#         self.all_smiles: list[str] = self.all_data[self.smiles_column_title].tolist()
-#         self.characters: list[str] = sorted(list(set(''.join(self.all_smiles))))
+        self.all_smiles: list[str] = self.all_data[self.smiles_column_title].tolist()
+        self.characters: list[str] = sorted(list(set(''.join(self.all_smiles))))
         
-#         self.vocab = character.CharacterVocab(characters=self.characters)
+        self.vocab = character.CharacterVocab(characters=self.characters)
 
-#         # Add beginning/end character to each SMILES string or molecules never end
-#         self.all_smiles = [
-#             self.vocab.bos.char 
-#             + smiles 
-#             + self.vocab.eos.char 
-#             for smiles in self.all_smiles
-#         ]  
+        # Add beginning/end character to each SMILES string or molecules never end
+        self.all_smiles = [
+            self.vocab.bos.char 
+            + smiles 
+            + self.vocab.eos.char 
+            for smiles in self.all_smiles
+        ]  
 
-#         # Encode all smiels strings and pad to appropriate length
-#         self.encoded_smiles = [torch.tensor(self.vocab.encode_text(smiles_string)) for smiles_string in self.all_smiles]
+        # Encode all smiels strings and pad to appropriate length
+        self.encoded_smiles = [torch.tensor(self.vocab.encode_text(smiles_string)) for smiles_string in self.all_smiles]
 
-#         # ToDo: Clean up this mess
-#         get_tensor = lambda x: torch.tensor(x.values.astype(float), dtype=torch.float32)
-#         self.indications_names = self.all_data.columns.drop(self.smiles_column_title).to_list()
-#         self.indications_tensor = get_tensor(self.all_data.drop(columns=[self.smiles_column_title]))
+        # ToDo: Clean up this mess
+        get_tensor = lambda x: torch.tensor(x.values.astype(float), dtype=torch.float32)
+        self.indications_names = self.all_data.columns.drop(self.smiles_column_title).to_list()
+        self.indications_tensor = get_tensor(self.all_data.drop(columns=[self.smiles_column_title]))
         
-#         # Shortcuts for sizes
-#         self.num_indications = self.indications_tensor.shape[-1]
+        # Shortcuts for sizes
+        self.num_indications = self.indications_tensor.shape[-1]
 
-#     def __len__(self) -> int:
-#         return len(self.all_smiles)
+    def __len__(self) -> int:
+        return len(self.all_smiles)
 
-#     def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
 
-#         indications = self.indications_tensor[idx, :]
+        indications = self.indications_tensor[idx, :]
 
-#         input_seq = self.encoded_smiles[idx][:-1]
-#         target_seq = self.encoded_smiles[idx][1:]
+        input_seq = self.encoded_smiles[idx][:-1]
+        target_seq = self.encoded_smiles[idx][1:]
 
-#         smiles_one_hot = torch.nn.functional.one_hot(
-#             input_seq, 
-#             num_classes=len(self.vocab)
-#         ).float()
+        smiles_one_hot = torch.nn.functional.one_hot(
+            input_seq, 
+            num_classes=len(self.vocab)
+        ).float()
         
-#         return smiles_one_hot, indications, target_seq
+        return smiles_one_hot, indications, target_seq
     
-#     def get_indications_tensor(self, indication: str):
-#         indication_index = self.indications_names.index(indication)
-#         indication_tensor = torch.zeros(len(self.indications_names))
-#         indication_tensor[indication_index] = 1
-#         return indication_tensor
+    def get_indications_tensor(self, indication: str):
+        indication_index = self.indications_names.index(indication)
+        indication_tensor = torch.zeros(len(self.indications_names))
+        indication_tensor[indication_index] = 1
+        return indication_tensor
 
-#     def get_dataloader(self, train: bool) -> DataLoader:
-#         # Create proper train/val split
-#         total_len = len(self)
-#         train_len = int(self.frac_train * total_len)  # 80% for training
+    def get_dataloader(self, train: bool) -> DataLoader:
+        # Create proper train/val split
+        total_len = len(self)
+        train_len = int(self.frac_train * total_len)  # 80% for training
         
-#         if train:
-#             subset = Subset(self, range(train_len))
-#         else:
-#             subset = Subset(self, range(train_len, total_len))
+        if train:
+            subset = Subset(self, range(train_len))
+        else:
+            subset = Subset(self, range(train_len, total_len))
             
-#         return DataLoader(
-#             subset,
-#             batch_size = self.batch_size,
-#             collate_fn = self.collate_fn,
-#             shuffle = train
-#         )
+        return DataLoader(
+            subset,
+            batch_size = self.batch_size,
+            collate_fn = self.collate_fn,
+            shuffle = train
+        )
 
-#     def collate_fn(self, batch):
-#         """
-#         Custom collate function to handle variable length sequences and pad them.
-#         """
-#         smiles, indications, targets = zip(*batch)
+    def collate_fn(self, batch):
+        """
+        Custom collate function to handle variable length sequences and pad them.
+        """
+        smiles, indications, targets = zip(*batch)
 
-#         # Pad the SMILES sequences
-#         smiles_padded = pad_sequence(smiles, batch_first=True, padding_value=self.vocab.pad.token)
+        # Pad the SMILES sequences
+        smiles_padded = pad_sequence(smiles, batch_first=True, padding_value=self.vocab.pad.token)
         
-#         # Pad the target sequences
-#         targets_padded = pad_sequence(targets, batch_first=True, padding_value=self.vocab.pad.token)
+        # Pad the target sequences
+        targets_padded = pad_sequence(targets, batch_first=True, padding_value=self.vocab.pad.token)
 
-#         # Stack the indications
-#         indications_stacked = torch.stack(indications)
+        # Stack the indications
+        indications_stacked = torch.stack(indications)
 
-#         return smiles_padded, indications_stacked, targets_padded
+        return smiles_padded, indications_stacked, targets_padded
 
-#     def train_dataloader(self) -> DataLoader:
-#         return self.get_dataloader(train=True)
+    def train_dataloader(self) -> DataLoader:
+        return self.get_dataloader(train=True)
 
-#     def val_dataloader(self) -> DataLoader:
-#         return self.get_dataloader(train=False)
+    def val_dataloader(self) -> DataLoader:
+        return self.get_dataloader(train=False)
 
 @attr.define
 class CharSMILESChEMBLIndications(Dataset):
@@ -135,10 +138,11 @@ class CharSMILESChEMBLIndications(Dataset):
     Character level SMILES database with disease indications
     """
 
+    # pd.DataFrame = ChemblDBIndications()._preprocess(ChemblDBIndications()._load_data(), max_length=max_length)
+    all_data: pd.DataFrame
+
     # Length and batch size for loading
     max_length: int = 512
-
-    all_data: pd.DataFrame = ChemblDBIndications()._preprocess(ChemblDBIndications()._load_data(), max_length=max_length)
     
     batch_size: int = 64
     frac_train: float = 0.8
